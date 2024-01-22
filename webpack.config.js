030f441f1dab59fbffb02fda7f3a6ae4b7ebaa34
dwarfdrizzle <dwarfdrizzle@gmail.com>
2024-01-22 13:44:26 +0000

    Cleaned up JS file conflicts and added webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  // Entry point of your application
+  entry: './frontend/static/js/main.js', // Adjust this path to where your main JS file is located
+  // Output configuration
+  output: {
+    path: path.resolve(__dirname, 'dist'), // The output directory
+    filename: 'bundle.js' // The name of the output file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Use Babel loader for JavaScript files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'] // Preset used for env setup
+          }
+        }
+      }
+    ]
+  },
+  // Optional: Configure webpack-dev-server
+  devServer: {
+    contentBase: './dist',
+  },
+};
